3bcfa451e42bd95c6f87e79e8975eacdfe72896d
Alex Miranda <alexmirandamoraes@gmail.com>
2024-02-09 16:10:58 +0100

    Create spring project

---
+++ src/test/java/io/github/alexmiranda/samples/temporal_poc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alexmiranda.samples.temporal_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
